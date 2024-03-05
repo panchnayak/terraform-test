4ac60223b33c3d250fb659677c4bf09081609269
Panchaleswar Nayak <panchaleswar@outlook.com>
2024-03-05 16:59:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,27 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+data "aws_ami" "ubuntu-linux-2204" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "example_server" {
-  ami           = "ami-04e914639d0cca79a"
+  ami                    = data.aws_ami.ubuntu-linux-2204.id
   instance_type = "t2.micro"
 
   tags = {
     Name = "PnayakEc2Example"
   }
 }
+
