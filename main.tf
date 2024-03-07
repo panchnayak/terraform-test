67778f36fc570f0e22533c2c804a7ddce2927429
Panchaleswar Nayak <panchaleswar@outlook.com>
2024-03-07 17:47:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "null_resource" "artifactory_server" {
       "cd $JFROG_HOME/artifactory/var/etc/",
       "touch ./system.yaml",
       "chown -R 1030:1030 $JFROG_HOME/artifactory/var",
-      "chmod -R 777 $JFROG_HOME/artifactory/var"
+      "chmod -R 777 $JFROG_HOME/artifactory/var",
       "docker run --name artifactory -v $JFROG_HOME/artifactory/var/:/var/opt/jfrog/artifactory -d -p 8081:8081 -p 8082:8082 releases-docker.jfrog.io/jfrog/artifactory-oss:7.77.5"
       ]
 
