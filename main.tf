5643cc65b1d195a4606669fd078fbf0058d05fbd
Panchaleswar Nayak <panchaleswar@outlook.com>
2024-03-07 20:42:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "null_resource" "artifactory_server" {
 provisioner "remote-exec" {
     inline = [
       "sudo chown -R 1030:1030 $JFROG_HOME/artifactory/var",
-      "chmod -R 777 $JFROG_HOME/artifactory/var",
+      "sudo chmod -R 777 $JFROG_HOME/artifactory/var",
       "docker run --name artifactory -v $JFROG_HOME/artifactory/var/:/var/opt/jfrog/artifactory -d -p 8081:8081 -p 8082:8082 releases-docker.jfrog.io/jfrog/artifactory-oss:7.77.5"
     ]
 
